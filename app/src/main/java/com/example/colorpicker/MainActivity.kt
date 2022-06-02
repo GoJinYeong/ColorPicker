5d3b1025e3391f18559e30a41988d0019c3c002d
GoJinYeong <gtjy55@gmail.com>
2022-06-02 14:43:11 +0900

    clone cording

--- app/src/main/java/com/example/colorpicker/MainActivity.kt
+++ app/src/main/java/com/example/colorpicker/MainActivity.kt
@@ -2,11 +2,40 @@ package com.example.colorpicker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import com.example.colorpicker.databinding.ActivityMainBinding
+import java.text.FieldPosition
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+    lateinit var selectColor: ColorObject
+
     // 뷰가 생성 되었을 때
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main) //xml file과 연결
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        loadColorSpinner()
+    }
+
+    private fun loadColorSpinner(){
+        selectColor = ColorList().defaultColor
+        binding.colorSpinner.apply {
+            adapter = ColorSpinerAdapater(applicationContext, ColorList().basicColors())
+            setSelection(ColorList().colorPosition(selectColor), false)
+            onItemSelectedListener = object : AdapterView.OnItemSelectedListener
+            {
+                override fun onItemSelected(p0: AdapterView<*>?, p1: View?, position: Int, p3: Long) {
+                    selectColor = ColorList().basicColors()[position]
+                }
+
+                override fun onNothingSelected(p0: AdapterView<*>?) {}
+            }
+
+
+        }
+
     }
 }
